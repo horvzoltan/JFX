83f6a3d3a99e2f05ddc37c2fe19258d6b6b07ef8
horvzoltan <horv.zoltan@hotmail.com>
2021-04-22 21:19:33 +0200

    Update

---
+++ src/view/AfterLogin.java
@@ -0,0 +1,39 @@
+package view;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AfterLogin {
+	@FXML
+	private TextField searchTextField;
+	@FXML
+	private TableView<String> table;
+	@FXML
+	private Button logoutButton;
+	@FXML
+	private Button addItemToCartButton;
+	@FXML
+	private Button openInventoryButton;
+	@FXML
+	private Button sellButton;
+
+	public void logout(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Login.fxml");
+
+	}
+
+	public void addItemToCart(ActionEvent event) throws IOException {
+
+	}
+
+	public void sellCurrentCart(ActionEvent event) throws IOException {
+
+	}
+
+}
