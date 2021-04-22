5277389ab2cd767cccb0e075263586c372dbb8ae
horvzoltan <horv.zoltan@hotmail.com>
2021-04-22 15:43:44 +0200

    AfterloginUIUpdate

--- src/sample/AfterLogin.java
+++ src/sample/AfterLogin.java
@@ -13,23 +13,28 @@ public class AfterLogin {
 	@FXML
 	private TextField searchTextField;
 	@FXML
-	private ScrollPane scrollPane;
-	@FXML
 	private TableView<String> table;
 	@FXML
-	private Button logout;
-	
-	
-	
-	
-	
+	private Button logoutButton;
+	@FXML
+	private Button addItemToCartButton;
+	@FXML
+	private Button openInventoryButton;
+	@FXML
+	private Button sellButton;
+
 	public void logout(ActionEvent event) throws IOException {
 		Main m = new Main();
 		m.changeScene("Login.fxml");
 
 	}
-	
-	
-	
-	
+
+	public void addItemToCart(ActionEvent event) throws IOException {
+
+	}
+
+	public void sellCurrentCart(ActionEvent event) throws IOException {
+
+	}
+
 }
